6848e357a8ed76c9b2ab6caa7808c70b73eb5349
pandabrand <pandabrand@gmail.com>
2012-05-21 13:31:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '44cb7ac23f072cc01c04932023c06f8e0aa7fce62a4294ad08d4f366d969bf4d3a7b8170d0e721ec581ed0d9377da6fa56432f82a9f7ca38f514fd32588606d2'
